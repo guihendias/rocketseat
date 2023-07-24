182391e14516b23bd2771d2439ddc64982109ad3
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2023-07-24 19:27:18 -0300

    adding gobarber react-native

---
+++ bootcamp/react-native/gobarber/ios/Pods/Target Support Files/Pods-gobarber-gobarberTests/Pods-gobarber-gobarberTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gobarber_gobarberTests : NSObject
+@end
+@implementation PodsDummy_Pods_gobarber_gobarberTests
+@end
---
+++ bootcamp/react-native/gobarber/ios/Pods/Target Support Files/Pods-gobarber/Pods-gobarber-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gobarber : NSObject
+@end
+@implementation PodsDummy_Pods_gobarber
+@end
